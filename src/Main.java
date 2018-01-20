e4ed27bcb301b16f6efeb33228ba478406f029ba
bacali <nasreddine.bacali95@gmail.com>
2018-01-20 21:28:56 +0100

    1.0.1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,14 @@
 
 import java.io.PrintWriter;
-
+import java.util.Scanner;
 
 public class Main {
 
     //FastScanner le plus rapide dans la lecture des données.
     //PrintWriter le plus rapide dans l'affichage des données.
     public static void main(String[] args) {
-        FastScanner in = new FastScanner(System.in);
+        Scanner in = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
-        
         
         out.close();
     }
