3dc536f9830613201cedb3cb7131a2b2e5274a45
bacali <nasreddine.bacali95@gmail.com>
2018-06-04 10:56:39 +0100

    .

--- src/com/eniso/acm/Graphs/mst/Kruskal.java
+++ src/com/eniso/acm/Graphs/mst/Kruskal.java
@@ -1,13 +1,40 @@
 package com.eniso.acm.Graphs.mst;
 
-import com.eniso.acm.UnionFind;
-import javafx.util.Pair;
+import com.eniso.acm.OtherCodes.UnionFind;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Kruskal {
+
+    static class Pair<T, R> {
+
+        T key;
+        R value;
+
+        public Pair(T key, R value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public T getKey() {
+            return key;
+        }
+
+        public void setKey(T key) {
+            this.key = key;
+        }
+
+        public R getValue() {
+            return value;
+        }
+
+        public void setValue(R value) {
+            this.value = value;
+        }
+
+    }
 
     public static void main(String[] args) {
         PrintWriter out = new PrintWriter(System.out);
@@ -23,7 +50,6 @@ public class Kruskal {
         //Minimum Spanning Tree
         EdgeList.sort(Comparator.comparingInt(Pair::getKey));
         //Maximum Spanning Tree
-//        EdgeList.sort((o1, o2) -> o2.getKey() - o1.getKey());
         int mst_cost = 0;
         UnionFind unionFind = new UnionFind(5);
         for (int i = 0; i < e; i++) {
